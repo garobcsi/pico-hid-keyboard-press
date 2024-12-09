ae3e9db86812cf6ca6a95c00eb3e34cd792297ac
Gáspár Róbert <gasparrobert0908@gmail.com>
2024-12-09 18:43:42 +0100

    Updated main.cpp and keyboard.h

--- src/main.cpp
+++ src/main.cpp
@@ -31,26 +31,11 @@
 #include "tusb.h"
 
 #include "usb_descriptors.h"
-#include "keyboard.h"
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
 //--------------------------------------------------------------------+
-
-// Blink pattern
-enum
-{
-    BLINK_NOT_MOUNTED = 250, // device not mounted
-    BLINK_MOUNTED = 1000,    // device mounted
-    BLINK_SUSPENDED = 2500,  // device is suspended
-};
-
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
-void led_blinking_task(void);
 void hid_task(void);
-
-KeyBoard keyboard;
 
 /*------------- MAIN -------------*/
 int main(void)
@@ -61,8 +46,6 @@ int main(void)
     while (1)
     {
         tud_task(); // tinyusb device task
-
-        led_blinking_task();
         hid_task(); // keyboard implementation
     }
 
@@ -86,7 +69,9 @@ static void send_hid_report(bool keys_pressed)
 
     if (keys_pressed)
     {
-        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keyboard.key_codes);
+        uint8_t key_codes[6] = {0};
+        key_codes[0] = 0x0A;
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, key_codes);
         send_empty = true;
     }
     else
@@ -100,34 +85,31 @@ static void send_hid_report(bool keys_pressed)
     }
 }
 
+
 // Every 10ms, we poll the pins and send a report
 void hid_task(void)
 {
+    board_led_write(false);
+
+    send_hid_report(false);
+
     // Poll every 10ms
-    const uint32_t interval_ms = 10;
-    static uint32_t start_ms = 0;
-
-    if (board_millis() - start_ms < interval_ms)
+    const uint32_t interval = 90000;
+    static uint32_t last_keypress_time = 0;
+    uint32_t current_time = board_millis();
+    
+    if (current_time - last_keypress_time < interval)
     {
-        return; // not enough time
+        return; // not enough time 
     }
-    start_ms += interval_ms;
+    last_keypress_time = current_time;
 
-    // Check for keys pressed
-    bool const keys_pressed = keyboard.update();
+    // send a report
+    send_hid_report(true);
+    send_hid_report(false);
 
-    // Remote wakeup
-    if (tud_suspended() && keys_pressed)
-    {
-        // Wake up host if we are in suspend mode
-        // and REMOTE_WAKEUP feature is enabled by host
-        tud_remote_wakeup();
-    }
-    else
-    {
-        // send a report
-        send_hid_report(keys_pressed);
-    }
+    board_led_write(true);
+    sleep_ms(50);
 }
 
 // Invoked when sent REPORT successfully to host
@@ -160,82 +142,4 @@ uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_t
 void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const *buffer, uint16_t bufsize)
 {
     (void)instance;
-
-    if (report_type == HID_REPORT_TYPE_OUTPUT)
-    {
-        // Set keyboard LED e.g Capslock, Numlock etc...
-        if (report_id == REPORT_ID_KEYBOARD)
-        {
-            // bufsize should be (at least) 1
-            if (bufsize < 1)
-                return;
-
-            uint8_t const kbd_leds = buffer[0];
-
-            if (kbd_leds & KEYBOARD_LED_CAPSLOCK)
-            {
-                // Capslock On: disable blink, turn led on
-                blink_interval_ms = 0;
-                board_led_write(true);
-            }
-            else
-            {
-                // Caplocks Off: back to normal blink
-                board_led_write(false);
-                blink_interval_ms = BLINK_MOUNTED;
-            }
-        }
-    }
-}
-
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device is mounted
-void tud_mount_cb(void)
-{
-    blink_interval_ms = BLINK_MOUNTED;
-}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-    blink_interval_ms = BLINK_NOT_MOUNTED;
-}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
-    (void)remote_wakeup_en;
-    blink_interval_ms = BLINK_SUSPENDED;
-}
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-    blink_interval_ms = BLINK_MOUNTED;
-}
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-    static uint32_t start_ms = 0;
-    static bool led_state = false;
-
-    // blink is disabled
-    if (!blink_interval_ms)
-        return;
-
-    // Blink every interval ms
-    if (board_millis() - start_ms < blink_interval_ms)
-        return; // not enough time
-    start_ms += blink_interval_ms;
-
-    board_led_write(led_state);
-    led_state = 1 - led_state; // toggle
 }
